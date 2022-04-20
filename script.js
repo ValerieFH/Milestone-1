2ded76d01de0c3199415eb55b5204ed65eb2b82b
Valerie Haupt <valre@Valeries-MacBook-Pro-2.local>
2022-04-19 21:23:20 -0500

    begin working on draw function

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
+//Global variable
+let shipX = 375
+let shipY = 700
+let stars = []
+let test = 0
+
+//Generate star field on load
+for (let i = 0; i < 80; i++){
+    stars.push(Math.floor(Math.random()*800))
+}
+
 //To do:
 //define initial canvas
 document.addEventListener('DOMContentLoaded', setupCanvas)
-document.addEventListener('DOMContentLoaded', drawShip)
+//document.addEventListener('DOMContentLoaded', draw)
+
+window.addEventListener('keypress', movement)
+
+function movement(e){
+    
+    let key = e.keyCode;
+    if (key === 119){
+        console.log("up")
+        shipY -= 3;
+        window.requestAnimationFrame(drawShip)
+    } else if (key === 115) {
+        console.log("down")
+    } else if (key === 100){
+        console.log("right")
+    } else if (key === 97){
+        console.log("left")
+    }else {
+        console.log(e.keyCode)
+    }
+
+}
 
 function setupCanvas(){
     let canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
+    //let ctx = canvas.getContext('2d');
     
     canvas.width = 800;
     canvas.height = 800;
 
-    for (let i = 10; i < 800; i+=10){
-        let x = Math.floor(Math.random()*800)
-        ctx.fillStyle = "white";
-        ctx.fillRect(x,i,4,4)
-    }
+    drawStarfield()
     
 }
 
@@ -29,9 +57,31 @@ function drawShip(){
     let testShip = new Image();
     testShip.src = 'assets/Player.png'
     testShip.onload = function(){
-        ctx.drawImage(testShip, 375, 375, 50, 50)
+        ctx.drawImage(testShip, shipX, shipY, 50, 50)
     }
 }
+
+function drawStarfield(){
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+
+    for (let i = 0; i < 80; i++){
+        let y = (i+1)*10
+        ctx.fillStyle = "white";
+        ctx.fillRect(stars[i],y,4,4)
+    }
+}
+
+function draw(){
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0,0,800,800)
+    drawStarfield()
+    drawShip()
+    window.requestAnimationFrame(draw)
+}
+
+window.requestAnimationFrame(draw)
 
 //
 //WEAPONS
