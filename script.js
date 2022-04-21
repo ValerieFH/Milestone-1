660359f89b0a3c0504c646f20e1c5e32c5e38fac
Valerie Haupt <valre@Valeries-MacBook-Pro-2.local>
2022-04-20 19:41:28 -0500

    added multi keypress functionality and loaded pixel image successfully

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 let shipX = 375
 let shipY = 700
 let stars = []
-let test = 0
+let keys = []
+let speed = 5
 
 //Generate star field on load
 for (let i = 0; i < 80; i++){
@@ -12,36 +13,62 @@ for (let i = 0; i < 80; i++){
 //To do:
 //define initial canvas
 document.addEventListener('DOMContentLoaded', setupCanvas)
-//document.addEventListener('DOMContentLoaded', draw)
 
-window.addEventListener('keypress', movement)
 
-function movement(e){
-    
-    let key = e.keyCode;
-    if (key === 119){
-        console.log("up")
-        shipY -= 3;
-        window.requestAnimationFrame(drawShip)
-    } else if (key === 115) {
-        console.log("down")
-    } else if (key === 100){
-        console.log("right")
-    } else if (key === 97){
-        console.log("left")
-    }else {
-        console.log(e.keyCode)
-    }
+// //Initial movement scheme
+// window.addEventListener('keypress', movement)
 
+// function movement(e){
+//     let speed = 15;
+//     let key = e.keyCode;
+//     if (key === 119){
+//         console.log("up")
+//         shipY -= speed;
+//         // window.requestAnimationFrame(drawShip)
+//     } else if (key === 115) {
+//         console.log("down")
+//         shipY += speed;
+//     } else if (key === 100){
+//         console.log("right")
+//         shipX += speed;
+//     } else if (key === 97){
+//         console.log("left")
+//         shipX -= speed;
+//     }else {
+//         console.log(e.keyCode)
+//     }
+// }
+
+//advanced movement stuff?
+
+window.addEventListener('keydown', keysDown, false)
+function keysDown(e) {
+    keys[e.key] = true;
 }
+
+window.addEventListener('keyup', keysUp, false)
+function keysUp(e) {
+    keys[e.key] = false;
+    
+}
+
+
+
+//pre loads images for rendering
+loadImages()
+
+function loadImages(){
+    player = new Image()
+    player.src = 'assets/Player.png'
+}
+
 
 function setupCanvas(){
     let canvas = document.getElementById('canvas');
-    //let ctx = canvas.getContext('2d');
     
     canvas.width = 800;
     canvas.height = 800;
-
+    
     drawStarfield()
     
 }
@@ -51,20 +78,18 @@ function setupCanvas(){
 //JS stuff
 //PLAYER
 //  create movement functionality
-function drawShip(){
+function drawPlayer(){
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
-    let testShip = new Image();
-    testShip.src = 'assets/Player.png'
-    testShip.onload = function(){
-        ctx.drawImage(testShip, shipX, shipY, 50, 50)
-    }
+    ctx.drawImage(player, shipX, shipY, 50, 50)
+    // ctx.fillStyle = "red"
+    // ctx.fillRect(shipX, shipY, 50, 50)
 }
 
 function drawStarfield(){
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
-
+    
     for (let i = 0; i < 80; i++){
         let y = (i+1)*10
         ctx.fillStyle = "white";
@@ -73,12 +98,23 @@ function drawStarfield(){
 }
 
 function draw(){
+    window.requestAnimationFrame(draw)
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
     ctx.clearRect(0,0,800,800)
+    if (keys.w && shipY > 0){
+        shipY -= speed;
+    } else if (keys.s && shipY < 750 ){
+        shipY += speed;
+    }
+    
+    if (keys.a && shipX > 0){
+        shipX -= speed
+    } else if (keys.d && shipX < 750){
+        shipX += speed
+    }
     drawStarfield()
-    drawShip()
-    window.requestAnimationFrame(draw)
+    drawPlayer()
 }
 
 window.requestAnimationFrame(draw)
